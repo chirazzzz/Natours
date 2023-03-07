4d123dbce4fba6f0a8c0250bb0e80f17afd0a50b
chirazzzz <chiragmehta.uk@gmail.com>
2023-03-07 17:35:23 +0000

    handling POST requests, works by overwriting tours-simple.json

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// express.json() is middleware that adds data from body to request obj
+app.use(express.json());
 
-app.post('/', (req, res) => {
-  res.send('You can post to this URL endpoint...');
-})
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this URL endpoint...');
+// })
 
 const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
 const toursObj = JSON.parse(tours);
@@ -24,6 +27,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours: toursObj,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = toursObj[toursObj.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  toursObj.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursObj),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          toursObj: newTour,
+        },
+      });
+      console.log('New tour saved');
+    }
+  );
 });
 
 const port = 3000;
