80939092521d1d67e641fb91914a5249fe643884
chirazzzz <chiragmehta.uk@gmail.com>
2023-03-15 15:51:35 +0000

    chaining checkBody middleware onto post route for createTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,6 +18,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if(!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Name or Price not submitted',
+    })
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
