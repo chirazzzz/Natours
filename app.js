4e4d58853e5c7faf2d1e3ec6b5f61e51cc8291d9
chirazzzz <chiragmehta.uk@gmail.com>
2023-03-15 17:59:11 +0000

    Setting up env variables and middleware to serve static files

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// outputs if we're in development or production
+console.log(process.env.NODE_ENV);
 /**** 1) Middleware - for all routes ****/
-
 // morgan also calls (req, res, next) callback func >>> https://github.com/expressjs/morgan/blob/master/index.js
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // express.json() is middleware that adds data from body to request obj
 app.use(express.json());
+
+// express.static() is middleware that allows static files to be served (anything in public directory)
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 🤯');
@@ -27,6 +33,5 @@ app.use((req, res, next) => {
 // Here's where we 'mount' our Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
