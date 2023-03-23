2ef87b14598ff7d4b6ba395629772af59830cf86
chirazzzz <chiragmehta.uk@gmail.com>
2023-03-23 09:24:32 +0000

    expanding the tour model and removing hardcoded ids from json data

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true, // trim only works on Strings and it removes any whitespace at start/end of string
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String], // this means an Array of type: String
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date], // this means an Array of type: Date
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
