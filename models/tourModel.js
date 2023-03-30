5ea7360741e3fd911086e785c39673e5f90b83bd
chirazzzz <chiragmehta.uk@gmail.com>
2023-03-30 15:44:13 +0100

    added virtual prop 'durationWeeks' to tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,69 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true, // trim only works on Strings and it removes any whitespace at start/end of string
+const tourSchema = new mongoose.Schema(
+  { // schema definition
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true, // trim only works on Strings and it removes any whitespace at start/end of string
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // this means an Array of type: String
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // this is how you exclude fields from being sent to client
+    },
+    startDates: [Date], // this means an Array of type: Date
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], // this means an Array of type: String
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // this is how you exclude fields from being sent to client
-  },
-  startDates: [Date], // this means an Array of type: Date
+  { // schema options
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// virtual property durationWeeks is created each time we use .get
+// cannot be used in query because it's not part of DB - just amending the response to client
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7; // used regular func coz we need to use 'this' keyword to point ot current document
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
