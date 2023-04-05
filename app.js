9ce53cea8140e1fc54ec998ab843a27abaf70a75
chirazzzz <chiragmehta.uk@gmail.com>
2023-04-05 13:27:18 +0100

    created AppError class and refactored error handlers in app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -29,26 +32,9 @@ app.use('/api/v1/users', userRouter);
 
 // app.all('*') handles any random routes
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on our server`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on our server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err); // whenever you pass arg into next() Express assumes that is an error
+  next(new AppError(`Can't find ${req.originalUrl} on our server`, 404)); // whenever you pass arg into next() Express assumes that is an error
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utils/appError.js
@@ -0,0 +1,17 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(
+      message
+    ); /* when extending parent class we call super() to call parent constructor
+                       passing message as arg because Error only accepts message parameter */
+
+    this.statusCode = statusCode;
+    // any 400, 401, 404 etc statusCodes are fails. Anything else is an error
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // will test for this and only send error messages for Operational errors
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
