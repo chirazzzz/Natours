8fe606625ce20d2d5533e368014f924c41e57e20
chirazzzz <chiragmehta.uk@gmail.com>
2023-04-26 10:44:11 +0100

    Implemented strong password management with bcryptjs

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   // schema definition
@@ -20,12 +21,31 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password!'],
     minlength: 8,
-    
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password!'],
+    validate: {
+      // This only works on CREATE and SAVE!!
+      validator: function (el) {
+        return el === this.password; // abc === abc ✅ abc === xyz ❌
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // checks if password hasn't been changed - returns next() and exits middleware
+  if (!this.isModified('password')) return next();
+
+  /* bcrypt.hash returns promise so need async/await and takes 
+    current password and adds CPU cost of 12 to encrypt it */
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // only require passwordConfirm for validation so making it undefined means it won't save to DB
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
