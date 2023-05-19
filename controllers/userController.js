c70cb7500e2b739ba4f131eec4724c624dc0d2ee
chirazzzz <chiragmehta.uk@gmail.com>
2023-05-19 09:32:13 +0100

    updateMe route working and only allows email & name to be changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -10,6 +19,41 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users: users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted field names that are not allowed to be updated (role, token)
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 2) Update user document
+  /* We saved password data with user.save() so validation would run every time on that sensitive (required) data 
+    updateMe will change non-sensitive data and often only one data point at a time so we don't want to validate each required field.
+    That's why it's better to use findByIdAndUpdate to alter non-sensitive data. It also doesn't enforce 'required' validation on passwords
+  const user = await User.findById(req.user.id);
+  user.name = 'Chirag';
+  await user.save(); */
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true, // we still want regular validation to run for non password data
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,8 @@ router.patch(
   authController.updatePassword // then password update is allowed
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
