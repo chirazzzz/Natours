d8aaf7c8cb42037dcf89fd5f9d3d21001f4d383c
chirazzzz <chiragmehta.uk@gmail.com>
2023-06-05 12:19:04 +0100

    implemented rate limiting with express-rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +9,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-/**** 1) Middleware - for all routes ****/
+/**** 1) Global Middleware - for all routes ****/
 // morgan also calls (req, res, next) callback func >>> https://github.com/expressjs/morgan/blob/master/index.js
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100, // if creating an more frequently used API this limit would need to be larger
+  windowMs: 60 * 60 * 1000,
+  message:
+    'Too many requests from this IP address, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // express.json() is middleware that adds data from body to request obj
 app.use(express.json());
