0393288e42eb894e28f5b682453485f3717549b5
chirazzzz <chiragmehta.uk@gmail.com>
2023-06-07 09:18:05 +0100

    added data sanitisation to prevent NoSQL injection & XSS attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,11 +35,17 @@ app.use('/api', limiter);
   express.json() is middleware that adds data from body to request obj */
 app.use(express.json({ limit: '10kb' }));
 
-/* e) Serving static fontVariantAlternates: 
+// e) Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// f) Data sanitization against XSS (Cross Site Scripting) attacks
+app.use(xss());
+
+/* g) Serving static fontVariantAlternates: 
   express.static() is middleware that allows static files to be served (anything in public directory) */
 app.use(express.static(`${__dirname}/public`));
 
-// f) Test middleware
+// h) Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
